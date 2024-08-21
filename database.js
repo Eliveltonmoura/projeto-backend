2a0756efce476115bec3ce237bb8d81853fedafe
Eliveltonmoura <eliveltonmoura112@gmail.com>
2024-08-21 17:07:36 -0300

    primeira versao

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('digital_college', 'nat', 'nat123', {
+const sequelize = new Sequelize('digital_college', 'eli', '123', {
   host: 'localhost',
   dialect: 'mysql'
 });
