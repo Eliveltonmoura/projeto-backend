162f89f95c091fe73d8e2f82b636c509270deef7
Eliveltonmoura <eliveltonmoura112@gmail.com>
2024-08-25 10:23:10 -0300

    rodou

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const { sequelize } = require('./models')
 const app = express()
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3306;
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}`)
